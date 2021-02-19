018938871ba48ebaa22024237e362a0b09c42ff1
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-02-19 12:33:27 -0300

    chore: eip :(

--- src/sendMetaTransaction.ts
+++ src/sendMetaTransaction.ts
@@ -20,7 +20,7 @@ import {
  * Send a meta transaction using a relay server
  * @param provider Which network you are connected to and therefore where the meta transaction will be signed
  * @param metaTransactionProvider Where the meta transaction will be executed
- * @param functionSignature Hexa of the function signature you want to execute
+ * @param functionSignature Hexa of the transaction data you want to execute
  * @param contractData Related contract data necessary to execute the transaction. Check getContract from this same package
  * @param partialConfiguration Configurable params like which relay server to use
  */
