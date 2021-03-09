73d82fedf5c58cccce038c59ee76b8e276b81a4b
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-03-09 17:55:19 -0300

    feat: get contract name

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -36,12 +36,27 @@ export function getContract(
 ): ContractData {
   const contract = contracts[contractName]
   if (!contract) {
-    throw new Error(`Invalid contract name: ${contractName}`)
+    throw new Error(`Could not get a valid contract for name: ${contractName}`)
   }
 
   if (!contract[chainId]) {
-    throw new Error(`Invalid chain ${chainId}`)
+    throw new Error(
+      `Could not get a valid contract for ${contractName} using chain ${chainId}`
+    )
   }
 
   return contract[chainId]!
+}
+
+export function getContractName(address: string): ContractName {
+  for (const contractName in contracts) {
+    for (const chainId in contracts[contractName]) {
+      const contract = contracts[contractName][chainId]
+
+      if (contract.address.toLowerCase() === address.toLowerCase()) {
+        return contractName as ContractName
+      }
+    }
+  }
+  throw new Error(`Could not get a valid contract name for address ${address}`)
 }
--- test/contracts.spec.ts
+++ test/contracts.spec.ts
@@ -1,14 +1,14 @@
 import { ChainId } from '@dcl/schemas'
 import { expect } from 'chai'
 import { abis } from '../src/abis'
-import { getContract } from '../src/contracts'
+import { getContract, getContractName } from '../src/contracts'
 import { ContractName } from '../src/types'
 
 describe('#getContract', () => {
   it('should throw if the contract name is not supported', () => {
     const contractName = 'Nonsense' as any
     expect(() => getContract(contractName, ChainId.ETHEREUM_MAINNET)).to.throw(
-      'Invalid contract name: Nonsense'
+      `Could not get a valid contract for name: ${contractName}`
     )
   })
 
@@ -51,10 +51,44 @@ describe('#getContract', () => {
       })
     })
 
-    it('should throw if the network is not supported', () => {
+    it('should throw if the chain is not supported', () => {
       expect(() =>
         getContract(ContractName.MANAToken, ChainId.ETHEREUM_ROPSTEN)
-      ).to.throw(`Invalid chain ${ChainId.ETHEREUM_ROPSTEN}`)
+      ).to.throw(
+        `Could not get a valid contract for ${ContractName.MANAToken} using chain ${ChainId.ETHEREUM_ROPSTEN}`
+      )
     })
+  })
+})
+
+describe('#getContractName', () => {
+  it('should return the contract name by address', () => {
+    const address = '0x882Da5967c435eA5cC6b09150d55E8304B838f45'
+    expect(getContractName(address)).to.eq(ContractName.MANAToken)
+  })
+
+  it('should support addresses from multiple chains', () => {
+    const addressMumbai = '0xa274eC4366703d31bF58F5053b708335FaAC2b52'
+    const addressMainnet = '0x8e5660b4ab70168b5a6feea0e0315cb49c8cd539'
+
+    expect(getContractName(addressMumbai)).to.eq(ContractName.Marketplace)
+    expect(getContractName(addressMainnet)).to.eq(ContractName.Marketplace)
+  })
+
+  it('should support all cases', () => {
+    const lowerCaseAddress = '0xe11663feb948892f2d2d966bfcb40ba2f5c02bb5'
+    const upperCaseAddress = '0XE11663FEB948892F2D2D966BFCB40BA2F5C02BB5'
+    const mixedCaseAddress = '0XE11663feb948892f2d2d966bfcb40BA2F5C02BB5'
+
+    expect(getContractName(lowerCaseAddress)).to.eq(ContractName.Forwarder)
+    expect(getContractName(upperCaseAddress)).to.eq(ContractName.Forwarder)
+    expect(getContractName(mixedCaseAddress)).to.eq(ContractName.Forwarder)
+  })
+
+  it('should throw if the address does not correspond to a contract', () => {
+    const address = '0xc0ffee254729296a45a3885639AC7E10F9d54979'
+    expect(() => getContractName(address)).to.throw(
+      `Could not get a valid contract name for address ${address}`
+    )
   })
 })
