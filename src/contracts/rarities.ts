7fad16e9d6bba44f5b16077e01d1d7d00d2c1500
Ignacio Mazzara <nachomazzara@gmail.com>
2021-04-29 15:36:36 -0300

    feat: change rarities contract

--- src/contracts/rarities.ts
+++ src/contracts/rarities.ts
@@ -5,7 +5,7 @@ export const rarities = {
   [ChainId.MATIC_MUMBAI]: {
     version: '1',
     abi: abis.Rarities,
-    address: '0xeF2d490521801CbC005FcB2f4ce0478FEEd95c89',
+    address: '0x540ed731A11dE041c09CA1bd4e5729dbF8A721Ea',
     name: 'Decentraland Rarities',
     chainId: ChainId.MATIC_MUMBAI
   }
