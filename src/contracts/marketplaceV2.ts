f75517df9b2c3ed806ff74c2f75d5b0448697996
Ignacio Mazzara <nachomazzara@gmail.com>
2021-11-20 21:09:13 -0300

    feat: update mkt v2 address

--- src/contracts/marketplaceV2.ts
+++ src/contracts/marketplaceV2.ts
@@ -5,7 +5,7 @@ export const marketplaceV2 = {
   [ChainId.MATIC_MUMBAI]: {
     version: '2',
     abi: abis.Marketplace,
-    address: '0x4e9c83F65dB67a6493d390f16C657e3e1dE37636',
+    address: '0x5A467398dfa9d5C663a656423A2D055f538198A4',
     name: 'Decentraland Marketplace',
     chainId: ChainId.MATIC_MUMBAI
   }
