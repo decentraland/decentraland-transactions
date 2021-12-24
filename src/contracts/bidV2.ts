66529d05e5526813c71c4d984332bd7e08a7fc73
Juan Cazala <juancazala@gmail.com>
2021-12-24 16:04:11 -0300

    fix: bid addresses

--- src/contracts/bidV2.ts
+++ src/contracts/bidV2.ts
@@ -5,14 +5,14 @@ export const bidV2 = {
   [ChainId.MATIC_MUMBAI]: {
     version: '2',
     abi: abis.Bid,
-    address: '0xa8f508624F4eFabD2A3a85099F15B0a3Fa06687a',
+    address: '0x78Dd92c8941dBC7BE54E2a9390D58aD28AD97afD',
     name: 'Decentraland Bid',
     chainId: ChainId.MATIC_MUMBAI
   },
   [ChainId.MATIC_MAINNET]: {
     version: '2',
     abi: abis.Bid,
-    address: '0x5cF39E64392C615FD8086838883958752a11B486',
+    address: '0xb96697FA4A3361Ba35B774a42c58dACcaAd1B8E1',
     name: 'Decentraland Bid',
     chainId: ChainId.MATIC_MAINNET
   }
