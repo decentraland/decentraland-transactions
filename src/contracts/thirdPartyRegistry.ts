a0e198c789798ff3b7f8b3f75e521353dbf25e1b
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-22 12:02:51 -0300

    chore: New TPR version

--- src/contracts/thirdPartyRegistry.ts
+++ src/contracts/thirdPartyRegistry.ts
@@ -5,7 +5,7 @@ export const thirdPartyRegistry = {
   [ChainId.MATIC_MUMBAI]: {
     version: '1',
     abi: abis.ThirdPartyRegistry,
-    address: '0x83cB8cd93698c0726AF6b5497540C9A1B410fFA2',
+    address: '0x57D5aDDE329C6D201Fcf674915AfF7bb7242C287',
     name: 'Decentraland Third Party Registry',
     chainId: ChainId.MATIC_MUMBAI
   }
