63cb877ffbacf392df77a7bfc8364e43f894a497
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-04-08 15:06:16 -0300

    chore: Update TPR abi and set matic mainnet config for tpr and chainlink oracle

--- src/contracts/chainlinkOracle.ts
+++ src/contracts/chainlinkOracle.ts
@@ -8,5 +8,12 @@ export const chainlinkOracle = {
     address: '0x508d4AC10F057beACb7Cef6f112e79075045C3C9',
     name: 'Decentraland ChainLink Oracle',
     chainId: ChainId.MATIC_MUMBAI
+  },
+  [ChainId.MATIC_MAINNET]: {
+    version: '1',
+    abi: abis.ChainlinkOracle,
+    address: '0xe18B1361d41afC44658216F3Dc27e48c2336e3c2',
+    name: 'Decentraland ChainLink Oracle',
+    chainId: ChainId.MATIC_MAINNET
   }
 }
