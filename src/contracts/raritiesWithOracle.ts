1be3d6d7eda08acf1061fe429910173f8ce7a3cd
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-04-19 15:47:56 -0300

    feat: Update abi

--- src/contracts/raritiesWithOracle.ts
+++ src/contracts/raritiesWithOracle.ts
@@ -4,14 +4,14 @@ import { abis } from '../abis'
 export const raritiesWithOracle = {
   [ChainId.MATIC_MUMBAI]: {
     version: '1',
-    abi: abis.Rarities,
+    abi: abis.RaritiesWithOracle,
     address: '0xb9957735bbe6D42585058Af11AA72da8eAD9043a',
     name: 'Decentraland Rarities with Oracle',
     chainId: ChainId.MATIC_MUMBAI
   },
   [ChainId.MATIC_MAINNET]: {
     version: '1',
-    abi: abis.Rarities,
+    abi: abis.RaritiesWithOracle,
     address: '0xA9158E22F89Bb3F69c5600338895Cb5FB81e5090',
     name: 'Decentraland Rarities with Oracle',
     chainId: ChainId.MATIC_MAINNET
