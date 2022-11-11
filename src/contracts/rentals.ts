ac5b8b2c299a8a4a458c22409c492dc5050192cd
Nando <24811313+fzavalia@users.noreply.github.com>
2022-11-11 15:23:08 -0300

    feat: Add mainnet rentals

--- src/contracts/rentals.ts
+++ src/contracts/rentals.ts
@@ -8,5 +8,12 @@ export const rentals = {
     address: '0x92159c78f0f4523b9c60382bb888f30f10a46b3b',
     name: 'Rentals',
     chainId: ChainId.ETHEREUM_GOERLI
+  },
+  [ChainId.ETHEREUM_MAINNET]: {
+    version: '1',
+    abi: abis.Rentals,
+    address: '0xcc94a49c7a81f59f07e0c381b929c4081c437de2',
+    name: 'Rentals',
+    chainId: ChainId.ETHEREUM_MAINNET
   }
 }
