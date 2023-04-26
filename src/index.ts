bc9e70c4515c6cb878f017d836b7833d98bac10a
Nando <24811313+fzavalia@users.noreply.github.com>
2023-04-25 22:12:14 -0300

    feat: Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import * as typechain from './typechain'
-
 export * from './contracts'
 export * from './sendMetaTransaction'
 export * from './types'
 export { isContract } from './utils'
 export * from './errors'
-export { typechain }
+export * from './typechain'
+export * from './typechain/factories'
