f9d77164a120224c2075606608a3508feb0995af
Nando <24811313+fzavalia@users.noreply.github.com>
2023-04-25 22:06:09 -0300

    feat: Export typechain

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import * as typechain from './typechain'
+
 export * from './contracts'
 export * from './sendMetaTransaction'
 export * from './types'
 export { isContract } from './utils'
 export * from './errors'
+export { typechain }
