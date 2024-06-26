04bd8c56b7401e546fce34725173e5289639200a
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-26 15:45:05 -0300

    Remove trailing comma

--- src/contracts/offChainMarketplace.ts
+++ src/contracts/offChainMarketplace.ts
@@ -8,5 +8,5 @@ export const offChainMarketplace = {
     address: '0xb901e30251CDb9CFaD2dDD4FDb4798D5B4312C69',
     name: 'Decentraland Off Chain Marketplace',
     chainId: ChainId.ETHEREUM_SEPOLIA
-  },
+  }
 }
