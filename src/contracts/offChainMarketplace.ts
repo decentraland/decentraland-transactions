fdc154a40ff40d333da5867bdd9a82652136cc9c
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-26 15:50:20 -0300

    feat: add new offchain marketplace (#95)

---
+++ src/contracts/offChainMarketplace.ts
@@ -0,0 +1,12 @@
+import { ChainId } from '@dcl/schemas'
+import { abis } from '../abis'
+
+export const offChainMarketplace = {
+  [ChainId.ETHEREUM_SEPOLIA]: {
+    version: '1',
+    abi: abis.OffChainMarketplace,
+    address: '0xb901e30251CDb9CFaD2dDD4FDb4798D5B4312C69',
+    name: 'Decentraland Off Chain Marketplace',
+    chainId: ChainId.ETHEREUM_SEPOLIA
+  }
+}
