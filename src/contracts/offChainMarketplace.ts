0ef254d24d48b1f45be0711a2b5b67c677abf642
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-07-04 11:57:29 -0300

    feat: add offchain marketplace in amoy (#96)

--- src/contracts/offChainMarketplace.ts
+++ src/contracts/offChainMarketplace.ts
@@ -3,10 +3,17 @@ import { abis } from '../abis'
 
 export const offChainMarketplace = {
   [ChainId.ETHEREUM_SEPOLIA]: {
-    version: '1',
+    version: '1.0.0',
     abi: abis.OffChainMarketplace,
     address: '0xb901e30251CDb9CFaD2dDD4FDb4798D5B4312C69',
-    name: 'Decentraland Off Chain Marketplace',
+    name: 'DecentralandMarketplaceEthereum',
     chainId: ChainId.ETHEREUM_SEPOLIA
+  },
+  [ChainId.MATIC_AMOY]: {
+    version: '1.0.0',
+    abi: abis.OffChainMarketplace,
+    address: '0xaF31A4620B95175C0F5ad6e968D77E7D1d3dd2A9',
+    name: 'DecentralandMarketplacePolygon',
+    chainId: ChainId.MATIC_AMOY
   }
 }
