8b1680c72aea3edc7b205b2fb270f24d8e5e7b2f
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-09-17 11:22:57 +0200

    feat: add mainnet contracts

--- src/contracts/offChainMarketplace.ts
+++ src/contracts/offChainMarketplace.ts
@@ -9,11 +9,25 @@ export const offChainMarketplace = {
     name: 'DecentralandMarketplaceEthereum',
     chainId: ChainId.ETHEREUM_SEPOLIA
   },
+  [ChainId.ETHEREUM_MAINNET]: {
+    version: '1.0.0',
+    abi: abis.OffChainMarketplace.ETHEREUM,
+    address: '0xb115402a3ad566363ec0ae690dd7fe0f5b96988f',
+    name: 'DecentralandMarketplaceEthereum',
+    chainId: ChainId.ETHEREUM_MAINNET
+  },
   [ChainId.MATIC_AMOY]: {
     version: '1.0.0',
     abi: abis.OffChainMarketplace.MATIC,
     address: '0x55f4d82b0f6a105b9f1959f10a660a4395d755ab',
     name: 'DecentralandMarketplacePolygon',
     chainId: ChainId.MATIC_AMOY
+  },
+  [ChainId.MATIC_MAINNET]: {
+    version: '1.0.0',
+    abi: abis.OffChainMarketplace.MATIC,
+    address: '0xedc74948a2fdcdaaa0c44853d2dcaf586f3f87c4',
+    name: 'DecentralandMarketplacePolygon',
+    chainId: ChainId.MATIC_MAINNET
   }
 }
