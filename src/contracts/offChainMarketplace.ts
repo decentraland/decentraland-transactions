5eda88039929f3c74568a51576f9537bec227d5d
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-09-30 18:23:14 -0300

    feat: change contract addr to new ones

--- src/contracts/offChainMarketplace.ts
+++ src/contracts/offChainMarketplace.ts
@@ -5,28 +5,28 @@ export const offChainMarketplace = {
   [ChainId.ETHEREUM_SEPOLIA]: {
     version: '1.0.0',
     abi: abis.OffChainMarketplace.ETHEREUM,
-    address: '0x347ECe82D01A2e7371fD3c1d2F6D4e85029B863f',
+    address: '0x54bdd3ffc1448bdab0251f05dd554eec803adb2f',
     name: 'DecentralandMarketplaceEthereum',
     chainId: ChainId.ETHEREUM_SEPOLIA
   },
   [ChainId.ETHEREUM_MAINNET]: {
     version: '1.0.0',
     abi: abis.OffChainMarketplace.ETHEREUM,
-    address: '0xb115402a3ad566363ec0ae690dd7fe0f5b96988f',
+    address: '0x2d6b3508f9aca32d2550f92b2addba932e73c1ff',
     name: 'DecentralandMarketplaceEthereum',
     chainId: ChainId.ETHEREUM_MAINNET
   },
   [ChainId.MATIC_AMOY]: {
     version: '1.0.0',
     abi: abis.OffChainMarketplace.MATIC,
-    address: '0x55f4d82b0f6a105b9f1959f10a660a4395d755ab',
+    address: '0x6ab20ae56673ed65f520b7be332aeb61b3ed727d',
     name: 'DecentralandMarketplacePolygon',
     chainId: ChainId.MATIC_AMOY
   },
   [ChainId.MATIC_MAINNET]: {
     version: '1.0.0',
     abi: abis.OffChainMarketplace.MATIC,
-    address: '0xedc74948a2fdcdaaa0c44853d2dcaf586f3f87c4',
+    address: '0x540fb08eDb56AaE562864B390542C97F562825BA',
     name: 'DecentralandMarketplacePolygon',
     chainId: ChainId.MATIC_MAINNET
   }
